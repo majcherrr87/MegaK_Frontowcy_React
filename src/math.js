8cce214e7a33a2836acaf92a4814688bf5e84020
Adrian Majcher <majcherrr87@gmail.com>
2024-02-07 12:57:37 +0100

    webpack config babel

--- src/math.js
+++ src/math.js
@@ -1,3 +1,4 @@
 export const add = (a, b) => {
+  throw new Error("Error");
   return a + b;
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
   entry: "./src/index.js",
   output: {
     filename: "bundle.js",
@@ -13,7 +16,24 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
     ],
   },
-  mode: "none",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+      filename: "index.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    port: 3000,
+    hot: true,
+  },
 };
