30bc8accbda61d9a3a5008c33b4cab10b79509bd
Adrian Majcher <majcherrr87@gmail.com>
2024-02-26 12:50:25 +0100

    zadaniedrawing task

--- my-vite-react-app/src/main.tsx
+++ my-vite-react-app/src/main.tsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React, { CSSProperties } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const styleBox: CSSProperties = {
+  width: 100,
+  height: 100,
+  margin: 5,
+  color: "red",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
+const colors = ["red", "blue", "green", "yellow", "purple"];
+
+const randomIndex = Math.round(Math.random() * 4);
+console.log(randomIndex, colors[randomIndex]);
+
+const promptValue = prompt("podaj color w postaci HEX");
+let goodColor;
+let infoColor;
+
+if (promptValue === null || promptValue.length < 7) {
+  infoColor = "Błędny kolor";
+  goodColor = "#000";
+} else {
+  goodColor = promptValue;
+  infoColor = promptValue;
+}
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <div
+      className="box1"
+      style={{ ...styleBox, backgroundColor: colors[randomIndex] }}
+    />
+    <div className="box2" style={{ ...styleBox, backgroundColor: goodColor }}>
+      {infoColor && infoColor}
+    </div>
+  </React.StrictMode>
+);
