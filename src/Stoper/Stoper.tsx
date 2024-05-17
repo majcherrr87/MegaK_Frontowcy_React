adbacf5cd4a7c47a5339c816deabc6dfeb1a1877
Adrian Majcher <majcherrr87@gmail.com>
2024-05-17 15:16:10 +0200

    another commit

--- src/Stoper/Stoper.tsx
+++ src/Stoper/Stoper.tsx
@@ -1,22 +1,23 @@
-import { useEffect, useState } from 'react';
-import { formatTime } from './formatTime';
+import { useEffect, useState } from 'react'
+import { formatTime } from './formatTime'
 
 export const Stoper = () => {
-  const [time, setTime] = useState(0);
+	const [time, setTime] = useState(0)
+	const kot = 'lisa'
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTime((prevValue) => prevValue + 100);
-    }, 100);
-    return () => clearInterval(interval);
-  }, []);
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setTime((prevValue) => prevValue + 100)
+		}, 100)
+		return () => clearInterval(interval)
+	}, [])
 
-  const { second, miliSecond } = formatTime(time);
-  console.log(`Kot ${second}`);
+	const { second, miliSecond } = formatTime(time)
+	console.log(`Kot ${second}`)
 
-  return (
-    <div>
-      Stoper {second}:{miliSecond}
-    </div>
-  );
-};
+	return (
+		<div>
+			Stoper {second}:{miliSecond}
+		</div>
+	)
+}
