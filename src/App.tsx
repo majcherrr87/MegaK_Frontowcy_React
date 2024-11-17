9b7b1079948bf56847489f9b34eb4a06b316b3c3
Adrian <majcherrr87@gmail.com>
2024-11-17 14:33:42 +0100

    input verification

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,75 @@
-import { yupResolver } from '@hookform/resolvers/yup'
 import { useForm } from 'react-hook-form'
-import { InferType, object, string } from 'yup'
-
-const loginSchema = object({
-	login: string().required().min(3),
-	password: string().required().min(3),
-})
-
-type LoginData = InferType<typeof loginSchema>
+import { OrderData, orderSchema } from './schemas/order'
+import { yupResolver } from '@hookform/resolvers/yup'
 
 export const App = () => {
 	const {
 		register,
 		handleSubmit,
+		watch,
 		formState: { errors },
-	} = useForm<LoginData>({
-		resolver: yupResolver(loginSchema),
+	} = useForm<OrderData>({
+		resolver: yupResolver(orderSchema),
 	})
-
-	const onSubmit = (data: LoginData) => {
-		console.log(data)
-	}
-
 	console.log(errors)
 
+	const onSubmit = (data: OrderData) => {
+		console.log('data', data)
+	}
+
+	const type = watch('payment.type')
+
 	return (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<input type="text" {...register('login')} />
-			<input type="password" {...register('password')} />
-			<button type="submit">Log in</button>
-		</form>
+		<>
+			<h1>Place your order</h1>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<div>
+					<h2>Basic data</h2>
+					<input type="text" {...register('basic.name')} />
+					<input type="text" {...register('basic.lastname')} />
+					<input type="number" {...register('basic.age')} />
+				</div>
+				<div>
+					<h2>Payment data</h2>
+					<label>
+						<input type="radio" value="card" {...register('payment.type')} />
+						Card
+					</label>
+					<label>
+						<input
+							type="radio"
+							value="transfer"
+							{...register('payment.type')}
+						/>
+						Transfer
+					</label>
+					{type === 'card' ? (
+						<div>
+							<label>
+								<input
+									type="radio"
+									value="visa"
+									{...register('payment.details.card')}
+								/>
+								VISA
+							</label>
+							<label>
+								<input
+									type="radio"
+									value="amex"
+									{...register('payment.details.card')}
+								/>
+								AMEX
+							</label>
+							<input type="text" {...register('payment.details.cardNumber')} />
+						</div>
+					) : null}
+					{type === 'transfer' ? (
+						<input type="text" {...register('payment.details.iban')} />
+					) : null}
+				</div>
+				<button type="submit">Place order</button>
+			</form>
+		</>
 	)
 }
---
+++ src/schemas/order.ts
@@ -0,0 +1,39 @@
+import { number, object, string, InferType } from 'yup'
+
+export const orderSchema = object({
+	basic: object({
+		name: string().required().min(3),
+		lastname: string().required().min(3),
+		age: number()
+			.required()
+			.transform((val) => val || 0)
+			.min(18),
+	}),
+	payment: object({
+		type: string().oneOf(['card', 'transfer']).required(),
+		details: object({
+			card: string(),
+			cardNumber: string(),
+			iban: string(),
+		}).when('type', {
+			is: 'card',
+			then: (schema) =>
+				schema.shape({
+					card: string().oneOf(['visa', 'amex']).required(),
+					cardNumber: string()
+						.required()
+						.when('card', {
+							is: 'visa',
+							then: (schema) => schema.matches(/^4/).length(16),
+							otherwise: (schema) => schema.matches(/^5/).length(15),
+						}),
+				}),
+			otherwise: (schema) =>
+				schema.shape({
+					iban: string().required().matches(/^PL/).length(34),
+				}),
+		}),
+	}),
+})
+
+export type OrderData = InferType<typeof orderSchema>
